e9d0c2563f644e699a2d0eb211b46455b207167e
Samkit Sheth <samkitsheth95@gmail.com>
2020-10-16 15:26:32 -0700

    Corrected server code

--- Assignment 2/server.c
+++ Assignment 2/server.c
@@ -63,10 +63,10 @@ int main(int argc, char const *argv[])
             perror("Cannot find UID for nobody");
         }
         setuid(pwd->pw_uid);
-        valread = read( new_socket , buffer, 1024);
-        printf("%s\n",buffer );
-        send(new_socket , hello , strlen(hello) , 0 );
-        printf("Hello message sent\n");
     }
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    send(new_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
     return 0;
 }
